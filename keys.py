a3ca23e4992838c485604bf377ae7d63b3bd87a7
peter ye <peteryej@gmail.com>
2020-06-17 08:05:50 -0700

    delete keys

--- keys.py
+++ keys.py
@@ -2,6 +2,6 @@
 
 BASE_URL = 'https://paper-api.alpaca.markets'
 
-API_KEY = 'PKSW2ZY64DXYH81475GA'
+API_KEY = 'put your key here'
 
-SECRET_KEY  = 'cxRijPItJbtgTzEAjvRDle6kV7wKveJ0Uc6vvdSX'
+SECRET_KEY  = 'put your key here'
